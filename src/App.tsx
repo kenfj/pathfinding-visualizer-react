7ee4e5f696e9e314aa2e7dd1ca5618132c8bb2b9
Ken Fujii <ken.fj@icloud.com>
2021-03-29 00:54:23 +0900

    add GithubCorner link to GitHub repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { makeStyles, Paper, Typography } from '@material-ui/core';
 import React from 'react';
+import GithubCorner from 'react-github-corner';
 import PathfindingVisualizer from './components/PathfindingVisualizer';
 
 const useStyles = makeStyles((theme) => ({
@@ -26,15 +27,20 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
+  const url = 'https://github.com/kenfj/pathfinding-visualizer-react'
 
   return (
     <Paper elevation={5} className={classes.paper}>
+      <GithubCorner href={url} />
       <h1>Pathfinding Visualizer</h1>
       <Typography variant="body1" gutterBottom>
         Click to create <span className={classes.wall}>WALL</span><br />
         Drag <span className={classes.start}>START</span> and <span className={classes.end}>END</span>
       </Typography>
       <PathfindingVisualizer nRows={20} nCols={30} />
+      <Typography variant="caption" display="block" gutterBottom>
+        source code: <a href={url}>{url}</a>
+      </Typography>
     </Paper >
   );
 }
